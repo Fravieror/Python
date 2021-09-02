905cd4db31638f4f5a6ebfe7e9a07e32e9ea05b8
FJ <lfjl@live.com>
2021-09-01 21:44:43 -0500

    Add request with parameters

--- api_client.py
+++ api_client.py
@@ -4,3 +4,11 @@ response = requests.get(url="http://api.open-notify.org/iss-now.json")
 print(response.json())
 longitude = response.json()["iss_position"]["longitude"]
 print(longitude)
+
+
+parameters = {
+    "lat": 4.710989,
+    "log": -74.072090
+}
+response_sunrise = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+print(response_sunrise.json())
